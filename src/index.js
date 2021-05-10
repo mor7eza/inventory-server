cb03e7f53c8121d739d5d889b0eb1d8a56db5d4d
Morteza Aliyari <aliyari.mda@gmail.com>
2021-05-10 20:41:35 +0430

    initialize database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const typeDefs = require("./graphql/typeDefs");
 
@@ -8,4 +10,15 @@ const server = new ApolloServer({
 
 server.listen({ port: process.env.PORT || 5000 }).then(({ url }) => {
   console.log(`Server is running on ${url}`);
+  mongoose
+    .connect(
+      `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    )
+    .then(() => {
+      console.log("Database Connected!");
+    });
 });
